09c4e41dd3ff70868911005a473d91da65ea4025
Jian Cheng <pawnand.com@gmail.com>
2023-06-08 11:13:14 +0100

    modify

--- prompt.py
+++ prompt.py
@@ -1,11 +1,13 @@
-optimize_prompt=f'''
-Job Description:
-{job_description}
+def generate_optimize_prompt(job_description, part_of_resume_title,part_of_resume,other_requirements):
+    optimize_prompt=f'''
+    Job Description:
+    {job_description}
 
-{part_of_resume_title}:
-{part_of_resume}
+    {part_of_resume_title}:
+    {part_of_resume}
 
-please optimize the {part_of_resume_title} based on the Job Description,{other_requirements}
-'''
+    please optimize the {part_of_resume_title} based on the Job Description,{other_requirements}
+    '''
+    return optimize_prompt
 
-
+# print(generate_prompt("hello","hello1","hello2","hello3"))
